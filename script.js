e2483260470f2500af975ab550554d28ddf7ae7b
gurukul-education <gjhh54912@gmail.com>
2026-03-27 11:08:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -63,6 +63,13 @@ function toggleFullscreen() {
     }
 }
 
+const glow = document.querySelector(".cursor-glow");
+
+document.addEventListener("mousemove", (e) => {
+    glow.style.left = e.clientX + "px";
+    glow.style.top = e.clientY + "px";
+});
+
 
 
 
