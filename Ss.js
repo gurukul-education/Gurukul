d870c732f80bb59c95e04fe624f1ccdd638a5115
gurukul-education <gjhh54912@gmail.com>
2026-04-01 09:07:03 +0530

    Update Ss.js

--- Ss.js
+++ Ss.js
@@ -1,1 +1,14 @@
 
+// SCROLL ANIMATION
+const elements = document.querySelectorAll('.slide-up, .slide-left, .slide-right');
+
+window.addEventListener('scroll', () => {
+    elements.forEach(el => {
+        const position = el.getBoundingClientRect().top;
+        const screenHeight = window.innerHeight;
+
+        if(position < screenHeight - 100) {
+            el.classList.add('active');
+        }
+    });
+});
